b55603f0ada4f02cd3e34ab0a24156b229c34841
Marco Botello <marco.botello@csu.fullerton.edu>
2021-02-09 21:03:53 -0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,12 @@
-var DETAIL_IMAGE_SELECTOR= '[data-image-role="target"]';
-var DETAIL_TITLE_SELECTOR= '[data-image-role="title"]';
-var THUMBNAIL_LINK_SELECTOR= '[data-image-role="trigger"]';
-function setDetails(imgUrl,titleText){
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails(imgUrl, titleText) {
     'use strict';
     var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
     detailImage.setAttribute('src', imageUrl);
-    
+
     var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
     detailTitle.textContent = 'You Should Be Dancing';
 }
